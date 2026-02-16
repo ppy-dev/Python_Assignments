002e5dd2aff61508b0de0f1bfa24b48e36ba7866
ppy-dev <PRITYADAV2020@GMAIL.COM>
2026-02-16 12:09:16 +0530

    Add files via upload

---
+++ Number is Even or Odd.py
@@ -0,0 +1,5 @@
+num=int(input("Enter a number: "))
+if num%2==0:
+    print(num ," is an Even number")
+else:
+    print(num,"num is an Odd Number")
---
+++ Sum of Integers from 1 to 50 Using a Loop.py
@@ -0,0 +1,10 @@
+
+# Initialize sum variable
+total_sum = 0
+
+# Iterate from 1 to 50 (both included)
+for number in range(1, 51):
+    total_sum += number   # Add each number to total_sum
+
+# Display the final sum
+print("The sum of numbers from 1 to 50 is:", total_sum)
